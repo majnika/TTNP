5d8658daf588e6e1e72cee3c215557f4ce239e84
Filip Štrba <filipko.strba@gmail.com>
2020-09-22 19:34:01 +0200

    Moved a lot of things here; Introduced system of queues; Added Front-End functions for queues

--- connection.py
+++ connection.py
@@ -1,23 +1,51 @@
 from datetime import datetime
 from typing import Tuple
+from cryptography.fernet import Fernet
+from queue import Queue
+from packet import Packet
 
 class Connection:
 
-    def __init__(self, addr: Tuple[str, int] ,TTL:float) -> None:
+    def __init__(self, addr: Tuple[str, int], server: str, TTL:float, incoming_queue: "Queue[Packet]", outgoing_queue: "Queue[Packet]") -> None:
         self.addr: Tuple[str, int] = addr
-        self.state: str = "Initiated"
-        self.last_hb: datetime
+        self.server: str = server
+        self._state: str = "Initiated"
+        self._q_IN = incoming_queue
+        self._q_OUT = outgoing_queue
+        self._last_hb: datetime
         self.TTL: float = TTL
+        self._f: Fernet
+        self._hanshake_sequence: "list[str]" = ["CHI","CDH","CCC"]
 
-    #await received("CDH")
+    def handshake(self):
+        pack = Packet("SHI","Hello",self.server)
+        pack.packed_data = pack.contents.encode() + '\n'.encode()
+        breakpoint()
+        pack.addr = self.addr
+        self.ship(pack)
+
+    def ship(self, pack: Packet) -> None:
+        self._q_OUT.put(pack)
+
+    def pack(self, to_pack: Packet) -> Packet:
+        msg: bytes = bytes()
+        msg += to_pack.flag.encode()
+        msg += self._f.encrypt((to_pack.data).encode())   
+        msg += ('#'*(251 - len(msg))).encode()
+        msg += to_pack.server.encode()
+        msg += "\n".encode()
+        to_pack.packed_data = msg
+        return to_pack
 
     @property
     def is_alive(self) -> bool:
-        return (datetime.now() - self.last_hb).seconds < self.TTL
+        return (datetime.now() - self._last_hb).seconds < self.TTL
 
-conn: Connection = Connection(("a",80),5.0)
+if __name__ == "__main__":
 
-conn.last_hb = datetime.now()
+    conn: Connection = Connection(("a",80),"0001",5.0,Queue(),Queue())
 
-while True:
-    print(conn.is_alive)
+    conn._last_hb = datetime.now() #type: ignore
+
+    while True:
+        print(conn.is_alive)
