3093343f100d1c8ef478b4ba7d4f8afc99218287
Filip Štrba <filipko.strba@gmail.com>
2020-09-22 19:34:34 +0200

    More consistent naming scheme for properties

--- packet.py
+++ packet.py
@@ -4,24 +4,32 @@ from typing import Tuple
 class Packet:
 
     addr: Tuple[str, int]
+    packed_data: bytes
 
     def __init__(self,flag: str, data: str, server: str) -> None:
         self.flag = flag
         self._str_data: str = data
         self.server = server
 
+    def __str__(self) -> str:
+        return self.contents
+
     @classmethod
     def from_bytes(cls,raw_bytes: bytes):
         raw: str = raw_bytes.decode()
         return cls(
         raw[0:3],
-        raw[3:251],
+        (raw[3:251]).rstrip('#'),
         raw[251:257]
         )
 
     @property
-    def raw(self) -> str:
+    def contents(self) -> str:
         return self.flag + self.data + self.server
+
+    @property
+    def raw_data(self) -> str:
+        return self._str_data
 
     @property
     def data(self) -> str:
@@ -29,4 +37,4 @@ class Packet:
 
     @data.setter
     def data(self,raw: str) -> None:
-        self._str_data = raw
+        self._str_data = raw.rstrip('#')
